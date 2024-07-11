3db6df3c7fe1d0c812bb3c9879dc117f5620839f
kilokomuli <kilokoemma175@gmail.com>
2024-07-11 16:26:12 +0300

    create table users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,2 +1,7 @@
 -- Creates  table Users
-CREATE TABLE users;
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
