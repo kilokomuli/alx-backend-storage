bd060cf89edd248cff8afb2a8615919a0007d2d7
emma <kilokoemma175@gmail.com>
2024-07-11 14:26:12 +0000

    add country

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -3,6 +3,6 @@
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
-    country ENUM ('US', 'US', 'TN') NOT NULL
+    name VARCHAR(255),
+    country ENUM ('US', 'CO', 'TN') NOT NULL
 );
