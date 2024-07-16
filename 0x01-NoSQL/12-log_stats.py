7a4d2ca9ebd71ce1f3e16b6a97fcbb5064a1b96c
Kiloko <kilokoemma175@gmail.com>
2024-07-16 13:44:16 -0700

    correction

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -8,14 +8,14 @@ def nginx_log_stats():
     client = MongoClient("mongodb://localhost:27017")
     db = client.logs
     collection = db.nginx
-    total_logs = collection.count_documents({})
+    total_logs = collection.cdocs({})
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    method_counts = {method: collection.count_documents({'method': method}) for method in methods}
-    status_check = collection.count_documents({'method': 'GET', 'path': '/status'})
+    m = {method: collection.cdocs({'method': method}) for method in methods}
+    status_check = collection.cdocs({'method': 'GET', 'path': '/status'})
     print(f"{total_logs} logs")
     print("Methods:")
     for method in methods:
-        print(f"\tmethod {method}: {method_counts[method]}")
+        print(f"\tmethod {method}: {m[method]}")
     print(f"{status_check} status check")
 
 
