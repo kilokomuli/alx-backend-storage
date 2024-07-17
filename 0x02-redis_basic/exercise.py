54d4b56bedc71ff0172aa06d4f673eec39adfbdf
kilokomuli <kilokoemma175@gmail.com>
2024-07-17 11:02:20 -0700

    task0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,25 +1,27 @@
+#!/usr/bin/env python3
+"""Module with statements of storing data with redis"""
 import redis
 import uuid
 from typing import Union
 
+
 class Cache:
     def __init__(self):
         """
-        Initializes an instance of the class.
+        Initializes an instance of the class cache.
         """
         self._redis = redis.Redis()
-        self._redis.flushdb
+        self._redis.flushdb(True)
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-            """
-            Stores the given data in Redis and returns the generated key.
-
-            Args:
-                data (Union[str, bytes, int, float]): The data to be stored in Redis.
-
-            Returns:
-                str: The generated key used to store the data in Redis.
-            """
-            key = str(uuid.uuid4())
-            self._redis.set(key, data)
-            return key
+        """
+        Stores the given data in Redis and returns the generated key.
+        Args:
+            data (Union[str, bytes, int, float]):
+            The data to be stored in Redis.
+        Returns:
+            str: The generated key used to store the data in Redis.
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
