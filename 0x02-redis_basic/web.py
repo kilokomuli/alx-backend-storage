8222c509aa275ddbbf5d70cfe2061c040aac8132
kilokomuli <kilokoemma175@gmail.com>
2024-07-19 05:02:26 -0700

    fix

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,30 @@
 #!/usr/bin/env python3
-"""
-Caching request module
-"""
+"""A module with tools for request caching and tracking."""
 import redis
 import requests
 from functools import wraps
 from typing import Callable
 
 
-def track_get_page(fn: Callable) -> Callable:
-    """ Decorator for get_page
-    """
-    @wraps(fn)
-    def wrapper(url: str) -> str:
-        """ Wrapper that:
-            - check whether a url's data is cached
-            - tracks how many times get_page is called
-        """
-        client = redis.Redis()
-        client.incr(f'count:{url}')
-        cached_page = client.get(f'{url}')
-        if cached_page:
-            return cached_page.decode('utf-8')
-        response = fn(url)
-        client.set(f'{url}', response, 10)
-        return response
-    return wrapper
+def cache_page(method: Callable) -> Callable:
+    """Caches the output of fetched data."""
+    @wraps(method)
+    def invoker(url: str) -> str:
+        """The wrapper function for caching the output."""
+        redis_store = redis.Redis()
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'{url}', result, 10)
+        return result
+    return invoker
 
 
-@track_get_page
+@cache_page
 def get_page(url: str) -> str:
-    """ Makes a http request to a given endpoint
+    """Returns the content of a URL after caching the request's response,
+    and tracking the request.
     """
-    response = requests.get(url)
-    return response.text
+    return requests.get(url).text
